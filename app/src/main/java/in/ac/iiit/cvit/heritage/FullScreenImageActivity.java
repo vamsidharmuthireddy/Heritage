a9e6d83d687bf5e755c4783a9238c82fe09d3875
Vamsidhar Muthireddy <vamsi.muthireddy@gmail.com>
2017-03-16 23:20:28 +0530

    Added gridview in gallery. Need to link images to InterestPointActivity

--- app/src/main/java/in/ac/iiit/cvit/heritage/FullScreenImageActivity.java
+++ app/src/main/java/in/ac/iiit/cvit/heritage/FullScreenImageActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 /**
  * Created by HOME on 16-03-2017.
@@ -17,6 +18,9 @@ public class FullScreenImageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_fullscreen_image);
 
         viewPager = (ViewPager) findViewById(R.id.pager);
